ef254fca052613789f67c767b709fc7e9f07a85c
alexviewnext78 <alexfja@hotmail.com>
2020-11-21 18:18:58 +0100

    Se ha corregido el texto quitando caracteres raros

--- src/ejercicioEclipse/EscribirTexto.java
+++ src/ejercicioEclipse/EscribirTexto.java
@@ -3,7 +3,7 @@ package ejercicioEclipse;
 public class EscribirTexto {
 
 	public static void main(String[] args) {
-		System.out.println("Hola ¿Qué tal?");
+		System.out.println("Hola �Que tal?");
 	}
 
 }
