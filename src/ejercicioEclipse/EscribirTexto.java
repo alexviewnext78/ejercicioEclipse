a0bda62213b190ec730d53c8205f7bc19d9c16e5
alexviewnext78 <60151359+alexviewnext78@users.noreply.github.com>
2020-11-21 18:23:26 +0100

    Añadida la fila ¿Me has escuchado?

--- src/ejercicioEclipse/EscribirTexto.java
+++ src/ejercicioEclipse/EscribirTexto.java
@@ -3,7 +3,7 @@ package ejercicioEclipse;
 public class EscribirTexto {
 
 	public static void main(String[] args) {
-		System.out.println("Hola �Que tal?");
+		System.out.println("Hola ¿Que tal? \n¿Me has escuchado?");
 	}
 
 }
